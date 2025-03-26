3998fd659305b76f888ddf9c2bb323f770323477
sariyaesc <escamillasara49@gmail.com>
2025-03-26 17:14:51 -0600

    feat: update everything

--- BACKEND/mi-api/index.js
+++ BACKEND/mi-api/index.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 3010;
+const http=require ('http')
+const {Server}=require("socket.io")
+const chatSocket= require("./sockets/chat")
 
 const userRoutes = require("./routes/users");
 const { testConnection } = require("./config/db");
 
-app.use("", userRoutes);
+app.use(cors())
 
-app.use(express.json()); //middleware para que los datos sean json
+const server=http.createServer(app)
+const io= new Server(server,{
+  cors: {
+    origin:'*',
+    methods:["GET","POST"]}
+  
+})
 
+io.on('connection',(socket)=>{
+      console.log(`Usuario conectado: ${socket.id}`);
+      socket.on('message',(msg)=>{
+          console.log(`Mensaje recibido: ${msg}`)
+          io.emit('message',msg) //mensaje a todos los usuarios conectados
+      });
 
-testConnection().then((connected) => {
-  if (connected) {
-    //inicializar servidor
-    app.listen(port, () => {
-      console.log("servidor escuchando en el puerto", port);
-    });
-  } else {
-    console.error("x no jala el mysql");
-  }
-});
+      socket.on('disconnect',()=>{
+          console.log(`Usuario desconectado: ${socket.id}`)
+      })
+  }) //abre conexion 
 
+server.listen(3010,()=>{
+  console.log("Servidor corriendo en http://localhost:3010")
+})
